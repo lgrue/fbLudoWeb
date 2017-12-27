fa2b14d994066b53df8585da6cff177a6062c155
LG <luca.grueninger@gmx.ch>
2017-12-27 02:27:43 +0100

    Project created

---
+++ fbLudoWebV3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="fbLudoWebV3.Global" Language="C#" %>
---
+++ fbLudoWebV3/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(fbLudoWebV3.Startup))]
+namespace fbLudoWebV3
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
